6c1673e3c9f1dadb9bf39035f1580c046a5b72b3
BSkando <124605801+BSkando@users.noreply.github.com>
2025-09-08 16:25:27 -0400

    Update username_provider.py

--- custom_components/googlefindmy/Auth/username_provider.py
+++ custom_components/googlefindmy/Auth/username_provider.py
@@ -6,7 +6,17 @@ from custom_components.googlefindmy.Auth.token_cache import get_cached_value, as
 
 username_string = 'username'
 
-async def get_username():
+def get_username():
+    username = get_cached_value(username_string)
+
+    if username is not None:
+        return username
+
+    # For Home Assistant integration, we need a Google email
+    # This should ideally be configured through the UI
+    return "user@gmail.com"  # Placeholder - should be configured
+
+async def async_get_username():
     username = await async_get_cached_value(username_string)
 
     if username is not None:
@@ -16,14 +26,6 @@ async def get_username():
     # This should ideally be configured through the UI
     return "user@gmail.com"  # Placeholder - should be configured
 
-def get_username_sync():
-    username = get_cached_value(username_string)
-
-    if username is not None:
-        return username
-
-    return "user@gmail.com"
-
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(get_username())
+    asyncio.run(async_get_username())
