6c6f6148378fe3f8503681e4a37fbe3e8cfe61ea
BSkando <124605801+BSkando@users.noreply.github.com>
2025-09-08 17:30:44 -0400

    Delete custom_components directory
    starting fresh, too many issues with this build

--- custom_components/googlefindmy/Auth/auth_flow.py
+++
@@ -1,7 +0,0 @@
-  #
-  #  GoogleFindMyTools - A set of tools to interact with the Google Find My API
-  #  Copyright © 2024 Leon Böttger. All rights reserved.
-  #
-
-  # Chrome driver functionality removed - not needed for Home Assistant integration
-  # that uses pre-generated secrets.json authentication
