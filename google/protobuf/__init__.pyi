d57120062d4bf1988f8037055cf9cc6e19f4da2f
Jens Leinenbach <1786119+jleinenbach@users.noreply.github.com>
2025-10-27 18:10:10 +0100

    Fix protobuf init exports

--- google/protobuf/__init__.pyi
+++ google/protobuf/__init__.pyi
@@ -3,6 +3,6 @@ from __future__ import annotations
 
 from types import ModuleType
 
-from .message import DecodeError, Message
+from .message import DecodeError as DecodeError, Message as Message
 
 text_format: ModuleType
