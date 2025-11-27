40cb6ae626583f5f601289269dc0e75d3c53c2b9
Jens Leinenbach <1786119+jleinenbach@users.noreply.github.com>
2025-11-27 08:10:12 +0100

    Sort map_view imports

--- custom_components/googlefindmy/map_view.py
+++ custom_components/googlefindmy/map_view.py
@@ -14,7 +14,8 @@ from urllib.parse import urlencode
 from aiohttp import web
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers import entity_registry as er
 from homeassistant.util import dt as dt_util
 
 from .const import (
